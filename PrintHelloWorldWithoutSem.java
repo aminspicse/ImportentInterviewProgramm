bf86c2ff32e04838377c83ba80c93848010f6b8e
AL <aminspicse@gmail.com>
2020-09-30 12:42:07 +0600

    Print Hello world without semicolon

---
+++ PrintHelloWorldWithoutSem.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package programm;
+
+/** print hello world without semicolon
+ * @author MD. AL AMIN
+ * portfolio: myspeec.com
+ * facebook: fb.com/amin.spicse
+ */
+public class PrintHelloWorldWithoutSem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(System.out.printf("Hello world") == null) {
+			
+		}
+
+	}
+
+}
